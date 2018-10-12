37cd61f9dd9181de210c7a75ba6e709063233890
lamdevhs <lam.dev.hs@gmail.com>
2018-10-12 16:10:16 +0200

    TP3, in class, v3

--- tp3/no_connexe.py
+++ tp3/no_connexe.py
@@ -1,18 +1,10 @@
 from vect import *
-from graphs import *
 
 def visitsVector(graph):
   return initVect(len(graph), 0)
 
 def size(graph):
   return len(graph) - 1
-
-# def gdfs(graph):
-#   visits = visitsVector(graph)
-#   nodes = nodesOf(graph)
-#   for i in nodes:
-#     if visits[i] == 0:
-#       dfs_rec(graph, i, visits)
 
 def isConnected(graph, i = 1):
   def dfs_rec(graph, i, visits, newvisit):
@@ -29,15 +21,16 @@ def isConnected(graph, i = 1):
   def dfs(graph, i, newvisit):
     dfs_rec(graph, i, visitsVector(graph), newvisit)
 
-  #global count
   count = 0
   def newvisit():
     nonlocal count
     count += 1
   dfs(graph, i, newvisit)
-  print (graph)
-  print (count, size(graph))
-  return count == size(graph)
+  isconnected = count == size(graph)
+  print(graph, count, isconnected)
+  return isconnected
   
+G2 = [[],[2,5,5], [1,3,4,4], [2,3,4], [2,2,3,5], [1,1,4]]
 print(isConnected(G2))
 print(isConnected(G2 + [[]]))
+# ^ on ajoute un unique point isole
